45845ff5813b62b0353e20d7cbd85f6ba587f87f
u6448938 <u6448938@INV094909.gmrio.pcrj.rj.gov.br>
2015-04-29 17:38:27 -0300

    pronto

---
+++ src/main/java/br/com/mrcsfelipe/spring/controller/HelloWorldController.java
@@ -0,0 +1,42 @@
+package br.com.mrcsfelipe.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.mrcsfelipe.spring.dao.JpaTarefaDao;
+import br.com.mrcsfelipe.spring.entity.Tarefa;
+
+@Transactional
+@Controller
+public class HelloWorldController {
+
+	@Autowired private JpaTarefaDao dao;
+	
+	
+	@RequestMapping("/hello")
+    public String hello(
+            @RequestParam(value="name", required=false, defaultValue="World") 
+            String name, 
+            Model model) {
+		
+        model.addAttribute("name", name);
+        return "helloworld";
+    }
+	
+	
+	@RequestMapping(value = "/addTarefa", method = RequestMethod.POST)
+	@Transactional
+	public String addPerson(Model model,  String name) {
+		Tarefa tarefa = new Tarefa();
+		tarefa.setNome(name);
+		dao.adiciona(tarefa);
+		
+		model.addAttribute("Cadastrado com sucesso", name);
+		return "redirect:/hello?name=cadastrado";
+	}
+}
---
+++ src/main/java/br/com/mrcsfelipe/spring/dao/JpaTarefaDao.java
@@ -0,0 +1,46 @@
+package br.com.mrcsfelipe.spring.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Service;
+
+import br.com.mrcsfelipe.spring.entity.Tarefa;
+
+@Service
+public class JpaTarefaDao{
+  
+  @PersistenceContext
+  EntityManager manager;
+
+  //sem construtor
+
+    public void adiciona(Tarefa tarefa) {
+      manager.persist(tarefa);
+    }
+
+    public void altera(Tarefa tarefa) {
+      manager.merge(tarefa);
+    }
+
+    public List<Tarefa> lista() {
+      return manager.createQuery("select t from Tarefa t")
+        .getResultList();
+    }
+ 
+    public Tarefa buscaPorId(Integer id) {
+      return manager.find(Tarefa.class, id);
+    }
+
+    public void remove(Tarefa tarefa) {
+      Tarefa tarefaARemover = buscaPorId(tarefa.getId());
+      manager.remove(tarefaARemover);
+    }
+
+	
+
+	
+   
+}
